a14aba76ff2fd263b9e5573502d4ada9643159d8
recosta32 <mercenarijava@gmail.com>
2018-10-25 15:29:25 +0200

    First push

--- app/src/main/java/com/android/gjprojection/roveedoll/MainActivity.java
+++ app/src/main/java/com/android/gjprojection/roveedoll/MainActivity.java
@@ -1,14 +1,31 @@
 package com.android.gjprojection.roveedoll;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.gjprojection.roveedoll.controls.DataBackground;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        final DataBackground dataBackground = findViewById(R.id.data_background);
+        final Button dataScale = findViewById(R.id.scaleButton);
+        dataScale.setText("x" + dataBackground.getxScale());
+        dataScale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataBackground.pushScale();
+                dataScale.setText("x" + dataBackground.getxScale());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/android/gjprojection/roveedoll/controls/DataBackground.java
@@ -0,0 +1,134 @@
+package com.android.gjprojection.roveedoll.controls;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.android.gjprojection.roveedoll.R;
+
+import java.util.ArrayList;
+
+public class DataBackground extends RelativeLayout implements View.OnTouchListener {
+    private static final int LINE_WIDTH = 10;
+    private static final int GRID_LINE_WIDTH = 1;
+    private static final int GRID_LINE_GAP = 200;
+    private static final int MAX_SCALE = 4;
+
+    ///////// PAINTERS /////////////
+    private @NonNull
+    Paint gridPaint;
+    private @NonNull
+    Paint linePaint;
+
+
+    ///////// ATTRIBUTES ////////////
+    private ArrayList<PointScaled> points = new ArrayList<>();
+    private int xScale = 1;
+
+    public DataBackground(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public DataBackground(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public DataBackground(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(@NonNull final Context context) {
+        setWillNotDraw(false);
+        gridPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        gridPaint.setColor(ContextCompat.getColor(context, R.color.gridLineColor));
+        linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        linePaint.setColor(ContextCompat.getColor(context, R.color.lineColor));
+        linePaint.setStrokeWidth(LINE_WIDTH);
+        setOnTouchListener(this);
+    }
+
+    public int getxScale() {
+        return xScale;
+    }
+
+    public void pushScale() {
+        this.xScale++;
+        if (this.xScale > MAX_SCALE) this.xScale = 1;
+        invalidate();
+    }
+
+    private void drawGrid(@NonNull Canvas canvas,
+                          final boolean vertical) {
+
+        long gap_sum = GRID_LINE_GAP / xScale;
+        while (gap_sum < (vertical ? getWidth() : getHeight())) {
+            canvas.drawLine(
+                    vertical ? gap_sum : 0,
+                    vertical ? 0 : gap_sum,
+                    vertical ? gap_sum + GRID_LINE_WIDTH : getWidth(),
+                    vertical ? getHeight() : gap_sum + GRID_LINE_WIDTH,
+                    gridPaint
+            );
+            gap_sum += GRID_LINE_GAP / xScale;
+        }
+    }
+
+    private void drawLine(@NonNull Canvas canvas) {
+        for (int i = 1; i < points.size(); i++) {
+            canvas.drawLine(
+                    points.get(i - 1).x / points.get(i - 1).scale,
+                    points.get(i - 1).y / points.get(i - 1).scale,
+                    points.get(i).x / points.get(i).scale,
+                    points.get(i).y / points.get(i).scale,
+                    linePaint
+            );
+        }
+    }
+
+    private void addPoint(final float x,
+                          final float y) {
+        points.add(new PointScaled(x, y, xScale));
+        invalidate();
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP: {
+                addPoint(motionEvent.getX(), motionEvent.getY());
+            }
+            break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawGrid(canvas, true);
+        drawGrid(canvas, false);
+        drawLine(canvas);
+    }
+
+    private static class PointScaled extends Point {
+        float scale;
+
+        PointScaled(float x, float y, float scale) {
+            super((int) x, (int) y);
+            this.scale = scale;
+        }
+    }
+}
